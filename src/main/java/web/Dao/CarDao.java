9ee37672b063b8ae43c430be1bba1693e41780b1
Ондрюшко <Ondrushko1986@yandex.ru>
2020-08-02 23:33:42 +0300

    1

--- src/main/java/web/Dao/CarDao.java
+++ src/main/java/web/Dao/CarDao.java
@@ -1,53 +1,54 @@
-//package web.Dao;
-//
-//import org.hibernate.Criteria;
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Repository;
+package web.Dao;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.Model.Car;
 //import web.Model.Car;
-////import web.Model.Car;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-//@Repository
-//public class CarDao implements Dao {
-//
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class CarDao implements Dao {
+
 //    @Autowired
-//    private SessionFactory sessionFactory;
-//
-//    private Session getSession() {
-//        return sessionFactory.getCurrentSession();
-//    }
-//
-//
-//    @Override
-//    public void addCar(Car car) {
-//        getSession().save(car);
-//    }
-//
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public List<Car> listCars() {
-//        Criteria criteria = getSession().createCriteria(Car.class);
-//        return (List<Car>) criteria.list();
-//    }
-//
-//    @Override
-//    public void saveOrUpdate(Car car) {
-//        getSession().saveOrUpdate(car);
-//    }
-//
-//    @Override
-//    public Car findCarById(int id) {
-//        Car car = getSession().get(Car.class, id);
-//        return car;
-//    }
-//
-//    @Override
-//    public void deleteCar(int id) {
-//        Car car = getSession().get(Car.class, id);
-//        getSession().delete(car);
-//    }
-//}
+    private SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+
+    @Override
+    public void addCar(Car car) {
+        getSession().save(car);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Car> listCars() {
+        Criteria criteria = getSession().createCriteria(Car.class);
+        return (List<Car>) criteria.list();
+    }
+
+    @Override
+    public void saveOrUpdate(Car car) {
+        getSession().saveOrUpdate(car);
+    }
+
+    @Override
+    public Car findCarById(int id) {
+        Car car = getSession().get(Car.class, id);
+        return car;
+    }
+
+    @Override
+    public void deleteCar(int id) {
+        Car car = getSession().get(Car.class, id);
+        getSession().delete(car);
+    }
+}
--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,62 +1,61 @@
-//package web.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.ModelAndView;
-//import web.Model.Car;
-//import web.service.CarService;
-//
-//
-//import java.util.List;
-//
-//@Controller
-//@RequestMapping("/cars")
-//public class CarController {
-//
-//    CarService carService;
-//
-//    @Autowired
-//    public CarController(CarService carService) {
-//        this.carService = carService;
-//    }
-//
-//    @RequestMapping("/")
-//    public ModelAndView home() {
-//        ModelAndView modelAndView = new ModelAndView("index");
-//        modelAndView.addObject("message", "Hello from my Spring MVC");
-//        return modelAndView;
-//    }
-//
-//    @RequestMapping(value = "/list", method = RequestMethod.GET)
-//    public ModelAndView list() {
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.Model.Car;
+import web.service.CarService;
+
+@Controller
+
+public class CarController {
+
+    CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+
+    @GetMapping(value = "/cars")
+    public Model getPage(Model model) {
 //        ModelAndView modelAndView = new ModelAndView("cars/list");
-//        List<Car> list = carService.ListCars();
+//        List<Car> list = carService.listCars();
 //        modelAndView.addObject("list", list);
-//        return modelAndView;
-//    }
-//
-//    @RequestMapping(value = "/add", method = RequestMethod.GET)
-//    public ModelAndView add() {
-//        ModelAndView modelAndView = new ModelAndView("cars/form");
-//        Car car = new Car();
-//        modelAndView.addObject("CarForm", car);
-//        return modelAndView;
-//    }
-//
-//    @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
-//    public ModelAndView update(@PathVariable("id") int id) {
-//        ModelAndView modelAndView = new ModelAndView("cars/form");
-//        Car car = carService.findCarById(id);
-//        modelAndView.addObject("CarForm", car);
-//        return modelAndView;
-//    }
-//
-//    @RequestMapping(value = "/save", method = RequestMethod.POST)
-//    public ModelAndView save(@ModelAttribute("CarForm") Car car) {
-//        carService.saveOrUpdate(car);
-//        return new ModelAndView("redirect:/cars/list");
-//    }
-//
-//
-//}
+        model.addAttribute("car1", getCar1());
+        model.addAttribute("car2", getCar2());
+        model.addAttribute("car3", getCar3());
+        return model;
+    }
+
+    private Car getCar1() {
+        Car car = new Car();
+        car.setId(1);
+        car.setColor("blue");
+        car.setName("volga");
+        car.setSpeed(250);
+        return car;
+    }
+
+    private Car getCar2() {
+        Car car = new Car();
+        car.setId(2);
+        car.setColor("black");
+        car.setName("bmw");
+        car.setSpeed(250);
+        return car;
+    }
+
+    private Car getCar3() {
+        Car car = new Car();
+        car.setId(3);
+        car.setColor("brown");
+        car.setName("audi");
+        car.setSpeed(250);
+        return car;
+    }
+
+
+}
